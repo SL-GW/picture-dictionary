52f895ffa7776ddda184645dac80d5a92d0d3103
SL-GW <162018972+SL-GW@users.noreply.github.com>
2024-07-28 16:03:50 +0200

    imported axios

--- src/Search.js
+++ src/Search.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
 import "./Search.css";
 
 export default function Search() {
+  const [word, setWord] = useState("");
+  const [images, setImages] = useState([]);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    searchImages(word);
+  }
+
+  function updateWord(event) {
+    setWord(event.target.value);
+  }
+
+  function searchImages(query) {
+    let apiKey = "c7eebf8ff7o8d23ac85774cb72ct0a65";
+    let apiUrl = `https://api.shecodes.io/images/v1/search?query=${word}&key=${apiKey}`;
+    axios.get(apiUrl).then((response) => {
+      setImages(response.data.photos);
+    });
+  }
+
   return (
     <div className="Search">
-      <form>
-        <input type="search" placeholder="Enter word" />
+      <form onSubmit={handleSubmit}>
+        <input type="search" placeholder="Enter word" onChange={updateWord} />
         <input type="submit" value="Search" />
       </form>
+      <div className="ImageResults">
+        {images.map((image, index) => (
+          <img key={index} src={image.src} alt={image.alt} />
+        ))}
+      </div>
     </div>
   );
 }
