dbe3699c9fcd7c081e91ac996473ce0259c330f8
SL-GW <162018972+SL-GW@users.noreply.github.com>
2024-07-28 16:10:05 +0200

    Added search engine

--- src/Search.js
+++ src/Search.js
@@ -20,6 +20,7 @@ export default function Search() {
     let apiKey = "c7eebf8ff7o8d23ac85774cb72ct0a65";
     let apiUrl = `https://api.shecodes.io/images/v1/search?query=${word}&key=${apiKey}`;
     axios.get(apiUrl).then((response) => {
+      console.log(response.data);
       setImages(response.data.photos);
     });
   }
@@ -32,7 +33,9 @@ export default function Search() {
       </form>
       <div className="ImageResults">
         {images.map((image, index) => (
-          <img key={index} src={image.src} alt={image.alt} />
+          <div key={index} className="ImageResult">
+            <img src={image.src.original} alt={image.alt} />
+          </div>
         ))}
       </div>
     </div>
